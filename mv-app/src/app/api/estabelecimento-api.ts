da02abdeeb776af7ab7e646f542150193a929469
Mauro Guterres <mauro.guterres@live.com>
2021-02-21 07:04:34 -0300

    remoção de dependencias não utilizadas

--- mv-app/src/app/api/estabelecimento-api.ts
+++ mv-app/src/app/api/estabelecimento-api.ts
@@ -1,4 +1,4 @@
-import {HttpClient, HttpHandler, HttpParams, HttpParamsOptions, HttpXhrBackend} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 import EstabelecimentoDTO from "../dto/estabelecimento-dto";
 
 
